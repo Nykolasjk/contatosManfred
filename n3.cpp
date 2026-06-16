0ea00080f1a13f1c5db8f7aca90451ff47bd1759
Nykolasjk <nykolas.kopp@catolicasc.edu.br>
2026-06-16 12:53:19 -0300

    add seção p/ excluir e melhorei a organização

--- n3.cpp
+++ n3.cpp
@@ -3,14 +3,14 @@
 #include <string.h>
 #include <windows.h>
 
-#define maxContatos 100
+#define maxContatos 1024
 
 	// significado typedef: a palavra que avisa o computador que vamos criar um apelido.
 	// significado struct: a palavra que agrupa as informações.
 	
 	typedef struct { // o typedef struct cria um "apelido/atalho" para um tipo de dado personalizado. ela junta várias variáveis diferentes dentro de uma só. 
 		
-	char  nome[60]; // os [] indicam o tamanho máximo do vetor de caracteres, aqui vai ser 59 + \0. esse (\0) é um caractere de finalização.
+	char nome[60]; // os [] indicam o tamanho máximo do vetor de caracteres, aqui vai ser 59 + \0. esse (\0) é um caractere de finalização.
 	char telefone[20];
     char email[50];
     char cidade[30];
@@ -20,6 +20,74 @@
 
 Contato agenda[maxContatos];
 int totalContatos = 0;
+
+void limpar();
+void incluirContato();
+void listarContatos();
+void buscarContato();
+void excluirContato();
+
+int main(void){
+	
+	int n=0;
+	
+	do{
+		
+		printf("\n====================================");
+        printf("\n      AGENDA DE CONTATOS");
+        printf("\n====================================");
+		printf("\n[1] - Incluir contato\n");
+		printf("[2] - Listar contatos\n");
+		printf("[3] - Buscar contato pelo nome\n");
+		printf("[4] - Excluir contato\n");
+		printf("[5] - Sair\n");
+		printf("Digite um numero de 1 a 5: ");
+		scanf("%i", &n);
+		limpar();
+		
+		switch(n){
+		
+		case 1:
+			
+			incluirContato();
+			
+		break;
+		
+		case 2:
+		
+			listarContatos();
+		
+		break;
+		
+		case 3:
+			
+			buscarContato();
+			
+		break;
+		
+		case 4:
+			
+			excluirContato();
+			
+		break;
+		
+		case 5:
+			printf("Obrigado por utilizar nosso programa, volte sempre!\nPrograma criado por Carla Canalle Mundel e Nykolas Jose Kopp.");
+		break;
+		
+		default:
+			printf("Valor invalido, tente novamente.");
+		break;
+			
+	}
+	
+		
+	}while(n != 5);
+	
+	
+	
+	return 0;
+}
 
 void limpar (){
 	
@@ -69,63 +137,67 @@ void listarContatos() {
     }
 }
 
+void buscarContato(){
+    char nome[60];
+    int encontrado = 0;
 
+	printf("\n============== BUSCA DE CONTATOS ==============\n");
 
-int main(void){
-	
-	int n=0;
-	
-	do{
-		
-		printf("\n====================================");
-        printf("\n      AGENDA DE CONTATOS");
-        printf("\n====================================");
-		printf("\n1 - Incluir contato\n");
-		printf("2 - Listar contatos\n");
-		printf("3 - Consultar contato pelo nome\n");
-		printf("4 - Excluir contato\n");
-		printf("5 - Excluir contato\n");
-		printf("Digite um numero de 1 a 5: ");
-		scanf("%i", &n);
-		limpar();
-		
-		switch(n){
-		
-		case 1:
-			
-			incluirContato();
-			
-		break;
-		
-		case 2:
-		
-			listarContatos();
-		
-		break;
-		
-		case 3:
-			
-		break;
-		
-		case 4:
-			
-		break;
-		
-		case 5:
-			printf("Obrigado por utilizar nosso programa, volte sempre!\nPrograma criado por Carla Canalle Mundel e Nykolas Jose Kopp.");
-		break;
-		
-		default:
-			printf("Valor invalido, tente novamente.");
-		break;
-			
-	}
-	
-		
-	}while(n != 5);
-	
-	
-	
-	return 0;
+    printf("\nDigite aqui o nome que deseja buscar: ");
+    scanf(" %[^\n]", nome);
+
+    for(int i = 0; i < totalContatos; i++) // faz o laço pra percorrer todos os contatos disponiveis
+    {
+        if(strcmp(nome, agenda[i].nome) == 0) // usei o strcmp pra comparar as strings, ele avalia caracter por caracter e ai valida. 
+        										//strcmp valida assim: se for 0, quer dizer que é igual ao contato que esta armazenado, se for diferente da erro
+        										// ele esta comparando o nome digitado na busca com os nomes na posição "i" da agenda
+        {
+            printf("\nNome: %s", agenda[i].nome);
+            printf("\nTelefone: %s", agenda[i].telefone);
+            printf("\nEmail: %s", agenda[i].email);
+            printf("\nCidade: %s", agenda[i].cidade);
+            printf("\nEmpresa: %s\n", agenda[i].empresa);
+
+            encontrado = 1;
+        }
+    }
+
+    if(!encontrado)
+    {
+        printf("\nContato nao encontrado.\n");
+    }
 }
 
+void excluirContato(){
+	char nome[60];
+    int encontrado = 0;
+    
+    printf("\n============== EXCLUSAO DE CONTATOS ==============\n");
+	
+	 printf("\nDigite aqui o nome do contato que deseja excluir: ");
+	 scanf(" %[^\n]", nome);
+	
+	for(int i = 0; i < totalContatos; i++)
+    {
+        if(strcmp(nome, agenda[i].nome) == 0) {
+           
+           for(int j = i; j < totalContatos - 1; j++)
+            {
+                agenda[j] = agenda[j + 1]; // troca a posição na agenda ao excluir o contato. se o 1ª contato for excluido, o 2ª pega sua posição, como se fosse uma esteira movendo pra esquerda os produtos
+            }
+
+            totalContatos--; // muda o total de contatos :p meio obvio mas e melhor pra gravar o que faz
+           
+           	printf("\nContato deletado com sucesso!\n");
+           
+            encontrado = 1;
+            
+        }
+    }
+    
+    
+    if(!encontrado)
+    {
+        printf("\nContato nao encontrado.\n");
+    }
+}
